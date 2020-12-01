a600d34924803b4c6bcdcdd60604bb21fa54565b
ArtuRito <arturoritoo+git@gmail.com>
2020-12-01 00:35:18 -0600

    modified to show allow up to 128 characters instead of 120

--- Develop/script.js
+++ Develop/script.js
@@ -14,110 +14,87 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
+  //recognize the click with this alert//
   alert("Let us make you a password!");
+  //create a loop that makes sure the user puts the correct information/values //
   do {
-    var characterInput = prompt("Password can be between 8-120 characters. How many characters would you like?");
+    //gives user parameters for their password
+    var characterInput = prompt("Password can be between 8-128 characters. How many characters would you like?");
+    //takes user input and converts it into integers so we can compare the value entered was a intenger later on
     var characterNumber = parseInt(characterInput);
-    console.log(characterInput)
-    console.log(characterNumber);
-    console.log(Number.isInteger(characterNumber));
+    //if user wants to cancel
     if (characterInput == null) {
+      //they'll receive cancellation notification
       alert("Password create canceled.");
-      return;   
-    }  else if  ((characterNumber < 8 || characterNumber > 120 || Number.isInteger(characterNumber) === false) && characterInput != null) {
+      //ends the remainder of the code once pressing cancel
+      return;
+      //checks to make sure the user input was correct, no numbers lesss than 8 or greater than 128, no other character except numbers 
+    }  else if  ((characterNumber < 8 || characterNumber > 128 || Number.isInteger(characterNumber) === false) && characterInput != null) {
+      //tells user their input is wrong if it does not meet the requirements
       alert("Your response is not appropriate. Choose a value between 8-120 for password length.")
     }
+    //repeats the loop until the requirements are met
+  }  while ((characterNumber < 8 || characterNumber > 128 || Number.isInteger(characterNumber) === false) && characterInput != null);
+
+  //these will be the questions that will be asked in the loop
+  var characterTypes = [
+    {q: "special characters", varName: true},
+    {q: "numeric characters", varName: true},
+    {q: "lowercase characters", varName: true},
+    {q: "uppercase characters", varName: true}]
+    console.log("How many conditions are there? " + characterTypes.length);
+    //loop that asks if they want to confirm a specific type of character for the password
+  for (var i = 0; i < characterTypes.length; i++) {
+    characterTypes[i].varName = confirm("Do you want to use " + characterTypes[i].q + "?");
+    console.log("Conditions for " + characterTypes[i].q + ": " + characterTypes[i].varName);
   }
-  while ((characterNumber < 8 || characterNumber > 120 || Number.isInteger(characterNumber) === false) && characterInput != null);
-  additionalConditions()
-  characterGenerator()
+  //sets up the characters for password conditions..//
+  var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "\/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "\`", "{", "|", "}", "~", "]"];
+  var numericCharacters = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  //takes all the vars for password generation and combines them into one array so that we can make loops with it later//
+  var characters = [
+    {char: specialCharacters},
+    {char: numericCharacters},
+    {char: uppercaseCharacters},
+    {char: lowercaseCharacters}]
+    //testing to see that it pulls correctly
+   //function characterGenerator() 
+  for (var j = 0; j < characterTypes.length; j++) {
+    //if they do not want a certain type of character, this loop will remove those arrays
+    if (characterTypes[j].varName === false) {
+      //sets that designated array to ""
+      characters[j].char = "";
+    } 
+      //test to confirm that it is showing the correct characters for all
+      console.log("Presented characters to be included: " + characterTypes[j].q + ": " + characters[j].char);
+  } //combines all active arrays that are true and not empty("")
+    var totalCharacters = [].concat(
+      characters[0].char || [],
+      characters[1].char || [], 
+      characters[2].char || [], 
+      characters[3].char || []
+      );
+    console.log("Total characters to choose from per cycle: " + totalCharacters.length);
+    console.log("Total Characters Array: " + totalCharacters);
+    console.log("Number of characters needed for password: " + characterNumber);
+    
+    var value = [];
+    
+  for (var k = 0; k < characterNumber; k++) {
+    var randomValue = Math.floor(Math.random() * totalCharacters.length);
+    var values = totalCharacters[randomValue];
+    value.push(values);
+  } password = value.join("");
+  console.log(value);
+  console.log(password);
+  return password
+    //randomValue[k] = Math.floor(Math.random() * totalCharacters.length);
+    //console.log(randomValue[k]);
+    //values[k] = totalCharacters[randomValue];
+    //console.log(values[k]);
+    //console.log("Generated number to match characters to be chosen from: " + randomValue);
+    //console.log("Resulting value from the value-match: " + values);
+      
 }
-    function characterGenerator() {//" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-      var characterLengths = {
-      specialCharacters : [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "\/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "\`", "{", "|", "}", "~", "]"],length: specialCharacters[0].length, 
-      numericCharacters : [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9], length: numericCharacters[0].length},
-      lowercaseCharacters : ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], length: lowercaseCharacters[0].length,
-      uppercaseCharacters : ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],  length: uppercaseCharacters[0].length
-      }
-      for (var j = 0; j < characterLengths.length; j++) {
-        if (characterTypes[j].varName === false) {
-          characterLengths[j][0] = "";
-          console.log(characterLengths[j][0])
-        }
-        var totalCharacters = specialCharacters[0].concat(numericCharacters[0], lowercaseCharacters[0], uppercaseCharacters[0]);
-        for (var k = 0; k < characterNumber; k++) {
-        var randomValue = Math.floor(Math.random() * totalCharacters.length);
-        console.log(totalCharacters);
-        var values = totalCharacters[randomValue[i]];
-        console.log(values[i]);
-        }
-
-      }
-    }      
-    function additionalConditions() {
-      var characterTypes = [
-        {q: "special characters", varName: true},
-        {q: "numeric characters", varName: true},
-        {q: "lowercase characters", varName: true},
-        {q: "uppercase characters", varName: true}]
-        console.log(characterTypes.length)
-        
-      for (var i = 0; i < characterTypes.length; i++) {
-        console.log(characterTypes.length)
-        characterTypes[i].varName = confirm("Do you want to use " + characterTypes[i].q + "?");
-        console.log(characterTypes[i]);
-      }
-    }
-
-//additionalConditions: function() {}
-  //var specialCharacters = confirm("Do you want to use special characters?");
-  //var numericCharacters = confirm("Do you want to use numeric characters?");
-  //var lowercaseCharacters = confirm("Do you want to use lowercase alphabetic characters?");
- // var uppercaseCharacters = confirm("Do you want to use uppercase alphabetic characters?");
-
-
-
-//var CharacterTypes = [
- // { specialCharacters, "special characters"},
- // { numericCharacters, "numeric characters"},
- // { lowercaseCharacters, "lowercase characters"},
- // { uppercaseCharacters, "uppercase characters}"}
-//];
-  // conditionsPassword();
-//}
-
-//function conditionsPassword() {
-  //var characterNumber = parseInt(characterInput);
-
-
-
- // } else { alert("Your response is not appropriate. Choose a value between 8-120 for password length.");
-
- //// }
-//}
-//}
-  //console.log(characterInput);
-//  console.log(characterNumber);
- // while (characterNumber > 7 || characterNumber < 121 || (Number.isInteger(characterNumber)) == true) {
-    
-    
- //// }
-
-  
- 
-
- // for (var i = 0; i < this.characterNumber; i++) {
-   // var value = Math.random();
-    //console.log(value);
-
-  
-
-//do{
- //   var selection = parseInt(window.prompt("Please enter a number from 1 to 100", ""), 10);
-//}while(isNaN(selection) || selection > 100 || selection < 1);
-
-
-
-
-
-//" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
